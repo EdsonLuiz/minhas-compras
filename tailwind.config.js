6840f2b18d15e8cc7f315e9d9e271f62f07b229e
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-02-14 12:10:28 -0300

    chore: configure colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,20 @@ module.exports = {
     darkMode: 'class', // or 'media' or 'class'
     theme: {
       colors: {
-        graywarm: colors.warmGray
+        transparent: 'transparent',
+        current: 'currentColor',
+  
+        black: colors.black,
+        white: colors.white,
+        gray: colors.coolGray,
+        graywarm: colors.warmGray,
+        red: colors.red,
+        yellow: colors.amber,
+        green: colors.emerald,
+        blue: colors.blue,
+        indigo: colors.indigo,
+        purple: colors.violet,
+        pink: colors.pink,
       },
       extend: {},
     },
